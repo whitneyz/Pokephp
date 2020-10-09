bba265a462237d52082585910cc942a0e11e578c
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-10-09 10:49:39 +0200

    added bootstrap pagination

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if (isset($pokName)) {
 //moves: there are more than 1 moves so try to make a loop for the moves
 //loop 4 times through array with random number
 
-var_dump($data);
+//var_dump($data);
 $allMoves = $data["moves"];
 $randomMoves = selectMoves($allMoves);
 
@@ -60,7 +60,7 @@ do {
 // left right buttons moet van de ene pokemon naar de andere kunnen gaan op basis van id of naam
 
 //extreme challenge: indexpage to show first 20 pokemon on first page, second 20 on next page etc
-
+//todo overview page is the page with all their features
 ?>
 
 <!doctype html>
@@ -71,12 +71,36 @@ do {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
 <body>
 <form action="" method="get">
     <input type="text" name="inputValue">
     <input type="submit" value="click">
 </form>
+
+<div class="row">
+    <div class="col-sm-4">.col-sm-4</div>
+    <div class="col-sm-4">.col-sm-4</div>
+    <div class="col-sm-4">.col-sm-4</div>
+</div>
+<nav aria-label="...">
+    <ul class="pagination">
+        <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1">Previous</a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item active">
+            <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+        </li>
+    </ul>
+</nav>
 <?php
 echo $data["name"];
 ?>
@@ -91,7 +115,15 @@ echo $randomMoves;
 <?php
 echo $data2;
 ?>
-
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
 
 </body>
 </html>
